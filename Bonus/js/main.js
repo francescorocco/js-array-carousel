1a77a31a1f0ccd8b8d52140561ff01d03748f1e6
Francesco Rocco <francescorocco97@gmail.com>
2023-02-28 17:45:27 +0100

    thumbnail completo

--- Bonus/js/main.js
+++ Bonus/js/main.js
@@ -1,9 +1,11 @@
 const imagesList = ['img/01.webp','img/02.webp','img/03.webp','img/04.webp','img/05.webp'];
+const tnList = imagesList
 
 const imageListDom = document.querySelector('.image-list');
+const tnListDom = document.querySelector('.thumbnails')
 
 let frameContainer = "";
-
+let tbContainer = "";
 
 for (let i = 0; i < imagesList.length; i++){
 
@@ -13,17 +15,27 @@ for (let i = 0; i < imagesList.length; i++){
 
     frameContainer += createdWrapper;
     console.log(frameContainer);
+
+    let createdtbContainer = `<div class="tn-wrap">
+        <img class="tn-image" src="${imagesList[i]}" alt="">
+        </div>`
+
+    tbContainer += createdtbContainer;
 }
 
 imageListDom.innerHTML = frameContainer;
+tnListDom.innerHTML = tbContainer;
 
 const imageWrapperDom = document.getElementsByClassName('image-wrapper');
+const tbWrapperDom = document.getElementsByClassName('tn-wrap');
+
 
 let selectedImage = 0;
 
 imageWrapperDom[selectedImage].classList.add('d-inBlock');
 console.log(imageWrapperDom);
 
+tbWrapperDom[selectedImage].classList.add('opacity');
 
 const upDom = document.querySelector('#bt-up');
 
@@ -36,19 +48,27 @@ upDom.addEventListener('click',
 
         if (selectedImage < imageWrapperDom.length - 1) {
 
+            tbWrapperDom[selectedImage].classList.remove('opacity');
+
             imageWrapperDom[selectedImage].classList.remove('d-inBlock');
 
             selectedImage++;
 
             imageWrapperDom[selectedImage].classList.add('d-inBlock');
 
+            tbWrapperDom[selectedImage].classList.add('opacity');
+
             downDom.classList.remove('d-none');
 
         }else if(selectedImage == (imageWrapperDom.length - 1)){
 
+            tbWrapperDom[selectedImage].classList.remove('opacity');
+
             imageWrapperDom[selectedImage].classList.remove('d-inBlock');
             
             selectedImage = 0;
+
+            tbWrapperDom[selectedImage].classList.add('opacity');
 
             imageWrapperDom[selectedImage].classList.add('d-inBlock');
         }
@@ -60,21 +80,30 @@ downDom.addEventListener('click',
 
         if (selectedImage > 0) {
 
+            tbWrapperDom[selectedImage].classList.remove('opacity');
+
             imageWrapperDom[selectedImage].classList.remove('d-inBlock');
 
             selectedImage--;
 
             imageWrapperDom[selectedImage].classList.add('d-inBlock');
 
+            tbWrapperDom[selectedImage].classList.add('opacity');
+
             upDom.classList.remove('d-none');
 
         }else if (selectedImage == 0){
             
+            tbWrapperDom[selectedImage].classList.remove('opacity');
+
             imageWrapperDom[selectedImage].classList.remove('d-inBlock');
 
             selectedImage = (imageWrapperDom.length - 1);
 
             imageWrapperDom[selectedImage].classList.add('d-inBlock');
+
+            tbWrapperDom[selectedImage].classList.add('opacity');
+
         }
     }
 );
